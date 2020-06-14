31c5c1f630fac60a6c432466a49a0907b00830e5
kdublam <kdublam@gmail.com>
2020-06-14 13:19:45 -0700

    Add app to capture GMB pages

--- dealership-capture.js
+++ dealership-capture.js
@@ -6,22 +6,21 @@ fs.readFile('dealerships.txt', function(err, data) {
     var array = data.toString().split("\n");
     for(i in array) {
         console.log("getting screenshot for: " + array[i])
-        getPic(array[i], i);
+        getPic(array[i]);
     }
 });
 
-async function getPic(x, num) {
+async function getPic(x) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   const domain = x.substring(12, x.lastIndexOf("."))
   await page.goto(x);
   await page.setViewport({
-    width: 1024,
-    height: 768,
+    width: 1400,
+    height: 1000,
     deviceScaleFactor: 1,
   });
   await page.screenshot({path: 'dealership_img/' + domain +'.png'});
 
   await browser.close();
 }
-
