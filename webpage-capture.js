7b11a838f2c73c7ee7728c89f8e2f14f451863df
kdublam <kdublam@gmail.com>
2020-08-05 16:54:15 -0700

    Add option to save pages with sub directories

--- webpage-capture.js
+++ webpage-capture.js
@@ -1,27 +1,26 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs')
 
-fs.readFile('GMB.txt', function(err, data) {
+fs.readFile('webpages.txt', function(err, data) {
     if(err) throw err;
     var array = data.toString().split("\n");
     for(i in array) {
         console.log("getting screenshot for: " + array[i])
-        getPic(array[i]);
+        getPic(array[i] , i);
     }
 });
 
-async function getPic(x) {
+async function getPic(x, num) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://www.google.com/search?q=' + x);
+  const domain = x.substring(12, x.lastIndexOf("."))
+  await page.goto(x);
   await page.setViewport({
-    // change this to capture different sizes
     width: 1400,
     height: 1000,
     deviceScaleFactor: 1,
   });
-  var name = x.replace(/\+/g, "_")
-  await page.screenshot({path: 'GMB_img/' + name +'.png'});
+  await page.screenshot({path: 'webpages_img/' + num +'.png'});
 
   await browser.close();
 }
